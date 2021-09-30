e0583019aa31e91163e93709b80952ea70ea6e25
hz <medo3578@gmail.com>
2021-09-30 09:59:08 +0200

    initial code for distribution

---
+++ get-week-payout.js
@@ -0,0 +1,26 @@
+const { ledgerManager } = require("./util");
+
+const main = async () => {
+  let file = "";
+  const ledgerResult = await ledgerManager.reloadLedger();
+  if (ledgerResult.error) {
+    throw {
+      type: "FAILURE",
+      error: `Error processing ledger events: ${ledgerResult.error}`,
+    };
+  }
+  const distributions = Array.from(ledgerManager.ledger.distributions());
+  const { allocations } = distributions[distributions.length - 1];
+  for (let { receipts } of allocations) {
+    for (let { id } of receipts) {
+      const account = ledgerManager.ledger.account(id);
+      const amount = ledgerManager.ledger.account(id).balance;
+      const address = account.payoutAddresses.get(
+        `{"type":"EVM","chainId":"137","tokenAddress":"0x9a71012B13CA4d3D0Cdc72A177DF3ef03b0E76A3"}`
+      );
+      if (address) {
+        file += `${address},${amount}\n`;
+      }
+    }
+  }
+};
---
+++ util.js
@@ -0,0 +1,24 @@
+const { sourcecred } = require("sourcecred");
+const fs = require("fs");
+const path = require("path");
+const { Ledger } = sourcecred.ledger.ledger;
+
+const createLedgerDiskStorage = (ledgerFilePath) => ({
+  read: async () => {
+    return Ledger.parse(fs.readFileSync(ledgerFilePath).toString());
+  },
+  write: async (ledger) => {
+    fs.writeFileSync(ledgerFilePath, ledger.serialize());
+  },
+});
+const { LedgerManager } = sourcecred.ledger.manager;
+
+const diskStorage = createLedgerDiskStorage(path.resolve("data/ledger.json"));
+
+const ledgerManager = new LedgerManager({
+  storage: diskStorage,
+});
+
+module.exports = {
+  ledgerManager,
+};
