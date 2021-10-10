3a84f632402f597b0fe40b72b8aafa36d549273b
hz <medo3578@gmail.com>
2021-10-10 11:48:31 +0200

    added accounting script and treasury sink

---
+++ do-accounting.js
@@ -0,0 +1,34 @@
+const process = require("process");
+const { ledgerManager } = require("./util");
+
+const [, , txUrl] = process.argv;
+
+const main = async () => {
+  const ledgerResult = await ledgerManager.reloadLedger();
+  if (ledgerResult.error) {
+    return {
+      type: "FAILURE",
+      error: `Error processing ledger events: ${ledgerResult.error}`,
+    };
+  }
+  const treasuryId = ledgerManager.ledger.accountByName("Treasury");
+  const distributions = Array.from(ledgerManager.ledger.distributions());
+  const { allocations } = distributions[distributions.length - 1];
+  for (let { receipts } of allocations) {
+    for (let { id } of receipts) {
+      const amount = ledgerManager.ledger.account(id).balance;
+      if (Number(amount) > 0 && id !== treasuryId) {
+        ledgerManager.ledger.transferGrain({
+          from: id,
+          to: treasuryId,
+          amount,
+          memo: txUrl,
+        });
+      }
+    }
+  }
+
+  await ledgerManager.persist();
+};
+
+main();
