89a0f94d3e232360de19bd532ddaf4863df6ca29
hz <medo3578@gmail.com>
2021-11-07 12:38:41 +0200

    accounting and stuff

--- get-week-payout.js
+++ get-week-payout.js
@@ -1,32 +1,38 @@
 const { ledgerManager } = require("./util");
 
 const main = async () => {
-  let file = "";
-  const ledgerResult = await ledgerManager.reloadLedger();
-  if (ledgerResult.error) {
-    throw {
-      type: "FAILURE",
-      error: `Error processing ledger events: ${ledgerResult.error}`,
-    };
-  }
-  const distributions = Array.from(ledgerManager.ledger.distributions());
-  const { allocations } = distributions[distributions.length - 1];
-  let total = 0;
-  for (let { receipts } of allocations) {
-    for (let { id } of receipts) {
-      const account = ledgerManager.ledger.account(id);
-      const amount = ledgerManager.ledger.account(id).balance;
-      const address = account.payoutAddresses.get(
-        `{"type":"EVM","chainId":"137","tokenAddress":"0x9a71012B13CA4d3D0Cdc72A177DF3ef03b0E76A3"}`
-      );
-      if (address && amount > 0) {
-        total += Number((amount / 1e18).toFixed(18)) * 1e18;
-        file += `${address},${(amount / 1e18).toFixed(18)}\n`;
+  try {
+    let file = "";
+    const ledgerResult = await ledgerManager.reloadLedger();
+    if (ledgerResult.error) {
+      throw {
+        type: "FAILURE",
+        error: `Error processing ledger events: ${ledgerResult.error}`,
+      };
+    }
+    const distributions = Array.from(ledgerManager.ledger.distributions());
+    const { allocations } = distributions[distributions.length - 1];
+    let total = 0;
+    for (let { receipts } of allocations) {
+      console.log(receipts);
+      receipts = receipts.sort((a, b) => +b.amount - +a.amount);
+      for (let { id } of receipts) {
+        const account = ledgerManager.ledger.account(id);
+        const amount = ledgerManager.ledger.account(id).balance;
+        const address = account.payoutAddresses.get(
+          `{"type":"EVM","chainId":"137","tokenAddress":"0x9a71012B13CA4d3D0Cdc72A177DF3ef03b0E76A3"}`
+        );
+        if (address && amount > 0) {
+          total += Number((amount / 1e18).toFixed(18)) * 1e18;
+          file += `${address},${(amount / 1e18).toFixed(18)}\n`;
+        }
       }
     }
+    console.log(file);
+    console.log(total);
+  } catch (error) {
+    console.log(error);
   }
-  console.log(file);
-  console.log(total);
 };
 
 main();
